d6a3d4e2a552f9b77630b86bdf1a568d535e893d
shankarp369 <shankar310105@gmail.com>
2026-01-03 21:36:46 +0530

    changes made

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,8 +1,8 @@
 
 
-AUTHOR          = '<your name>'  # use your name
-SITENAME        = 'title' # use title
-GITHUB_USERNAME = 'tactlabs' # use your username
+AUTHOR          = 'shankartce'  # use your name
+SITENAME        = 'My Python Notes' # use title
+GITHUB_USERNAME = 'shankartce' # use your username
 
 
 # Blogroll
@@ -12,8 +12,8 @@ LINKS = (
 
 # Social widget
 SOCIAL = (
-    ("GitHub", "https://github.com/<yourusername>/"),
-    ("LinkedIn", "<linkedin handle>"),
+    ("GitHub", "https://github.com/shankartce"),
+    ("LinkedIn", "https://www.linkedin.com/in/shankarp369/"),
 )
 
 ######## Advanced Settings (not recommended to to edited; take your own risk to touch the settings below) #############
